090bb0bb4f86d03a71ed68867a7fa684b1676171
omar <omar.tourabi7@gmail.com>
2023-09-06 23:39:02 +0200

    Remove comment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'node:path'
-/*  
-  - Ajouter du CSS 
-  - A build et tester avec le link (dossier dist) 
-  - Essayer dans le projet 14
- */
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
