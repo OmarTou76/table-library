5d970cdf906575df6657c2dd14406430bbbd40fb
omar <omar.tourabi7@gmail.com>
2023-10-02 20:37:20 +0200

    ref(SEO): Added htmlFor of select item per page

--- src/DataTable/Subcomponents/Pagination.jsx
+++ src/DataTable/Subcomponents/Pagination.jsx
@@ -7,9 +7,9 @@ export const Pagination = ({ itemsPerPage, showEntries, setShowEntries, data, cu
     return (
         <div className="pagination-container">
             <div style={{ display: "flex", gap: '1rem' }}>
-                <label>Rows per page:</label>
+                <label htmlFor='rows-per-page'>Rows per page:</label>
                 {itemsPerPage.length ?
-                    <select value={showEntries} onChange={(e) => {
+                    <select id='rows-per-page' value={showEntries} onChange={(e) => {
                         setShowEntries(parseInt(e.target.value))
                     }}>
                         {itemsPerPage.map(item => (
@@ -30,14 +30,17 @@ export const Pagination = ({ itemsPerPage, showEntries, setShowEntries, data, cu
             </div>
             <div className="pagination">
                 <button className="pagination-btn"
+                    aria-label='previous'
                     style={{ background: theme?.primary?.light, borderColor: theme?.primary?.dark }}
                     disabled={currentPage === 0} onClick={() => setCurrentPage(page => page - 1)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14.71 6.71a.996.996 0 0 0-1.41 0L8.71 11.3a.996.996 0 0 0 0 1.41l4.59 4.59a.996.996 0 1 0 1.41-1.41L10.83 12l3.88-3.88c.39-.39.38-1.03 0-1.41z" /></svg>
                 </button>
                 <span>{currentPage + 1}</span>
                 <button className="pagination-btn"
+                    aria-label='next'
                     style={{ background: theme?.primary?.light, borderColor: theme?.primary?.dark }}
                     disabled={(currentPage + 1 >= Math.ceil(data.filter(searchValues).length / showEntries))}
+
                     onClick={() => setCurrentPage((page) => page + 1)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9.29 6.71a.996.996 0 0 0 0 1.41L13.17 12l-3.88 3.88a.996.996 0 1 0 1.41 1.41l4.59-4.59a.996.996 0 0 0 0-1.41L10.7 6.7c-.38-.38-1.02-.38-1.41.01z" /></svg>
                 </button>
